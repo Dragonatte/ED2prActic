b8b99bc5ce61059d6e6d22f67177d0141e9f3dac
rodri <rodri@DESKTOP-J5E8BHE>
2022-02-21 22:55:00 +0100

    Setter titulo

---
+++ ED/src/RodrigoMurilloMusica.java
@@ -0,0 +1,12 @@
+public class RodrigoMurilloMusica {
+	public String titulo;
+	public int nMinutos;
+	public String nombreHobby;
+	
+	public RodrigoMurilloMusica(String titulo, int nMinutos, String nombreHobby) {
+		super();
+		this.titulo=titulo;
+		this.nMinutos=nMinutos;
+		this.nombreHobby=nombreHobby;
+	}
+}
